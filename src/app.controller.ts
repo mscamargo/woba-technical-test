608d9eb1ff8d020265d8462b533e7b66c299c8fb
mscamargo <mscamargo.dev@gmail.com>
2023-10-19 12:42:47 -0300

    refactor: move logic to a service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,68 +1,21 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Headers,
-  Post,
-  UnauthorizedException,
-  UnprocessableEntityException,
-} from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post } from '@nestjs/common';
 import { CreateAppointmentDto } from './create-appointment.dto';
-import { AppointmentsRepository } from './appointments.repository';
-import { AppointmentCreatedEvent } from './appointment-created.event';
-import { EventEmitter2 } from '@nestjs/event-emitter';
+import { AppService } from './app.service';
 
 @Controller({
   path: 'appointments',
   version: '1',
 })
 export class AppController {
-  constructor(
-    private readonly appointmentsRepository: AppointmentsRepository,
-    private readonly eventEmitter: EventEmitter2,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Post()
   async create(@Body() createAppointmentDto: CreateAppointmentDto) {
-    const now = new Date();
-
-    const [startHour, startMinute] = createAppointmentDto.start_time.split(':');
-    const [endHour, endMinute] = createAppointmentDto.end_time.split(':');
-
-    const appointmentStartTime = new Date(createAppointmentDto.date);
-    appointmentStartTime.setHours(Number(startHour), Number(startMinute));
-
-    const appointmentEndTime = new Date(createAppointmentDto.date);
-    appointmentEndTime.setHours(Number(endHour), Number(endMinute));
-
-    if (appointmentStartTime < now) {
-      throw new UnprocessableEntityException(
-        'the appointment must start in the future',
-      );
-    }
-
-    if (appointmentStartTime > appointmentEndTime) {
-      throw new UnprocessableEntityException(
-        'the start of the appointment must be before the end',
-      );
-    }
-
-    const appointment =
-      await this.appointmentsRepository.create(createAppointmentDto);
-
-    this.eventEmitter.emit(
-      'appointment.created',
-      new AppointmentCreatedEvent(appointment.id, appointment.created_at),
-    );
-
-    return appointment;
+    return this.appService.createAppointment(createAppointmentDto);
   }
 
   @Get()
-  async list(@Headers('x-user-id') userId: string) {
-    if (!userId) {
-      throw new UnauthorizedException();
-    }
-    return this.appointmentsRepository.findByUserId(userId);
+  async list(@Headers('x-user-id') userId?: string) {
+    return this.appService.getAppointmentsByUserId(userId);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { CreateAppointmentDto } from './create-appointment.dto';
+import { AppointmentsRepository } from './appointments.repository';
+import { EventEmitter2 } from '@nestjs/event-emitter';
+import { AppointmentCreatedEvent } from './appointment-created.event';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly appointmentsRepository: AppointmentsRepository,
+    private readonly eventEmitter: EventEmitter2,
+  ) {}
+
+  async createAppointment(createAppointmentDto: CreateAppointmentDto) {
+    const now = new Date();
+
+    const [startHour, startMinute] = createAppointmentDto.start_time.split(':');
+    const [endHour, endMinute] = createAppointmentDto.end_time.split(':');
+
+    const appointmentStartTime = new Date(createAppointmentDto.date);
+    appointmentStartTime.setHours(Number(startHour), Number(startMinute));
+
+    const appointmentEndTime = new Date(createAppointmentDto.date);
+    appointmentEndTime.setHours(Number(endHour), Number(endMinute));
+
+    if (appointmentStartTime < now) {
+      throw new UnprocessableEntityException(
+        'the appointment must start in the future',
+      );
+    }
+
+    if (appointmentStartTime > appointmentEndTime) {
+      throw new UnprocessableEntityException(
+        'the start of the appointment must be before the end',
+      );
+    }
+
+    const appointment =
+      await this.appointmentsRepository.create(createAppointmentDto);
+
+    this.eventEmitter.emit(
+      'appointment.created',
+      new AppointmentCreatedEvent(appointment.id, appointment.created_at),
+    );
+
+    return appointment;
+  }
+
+  async getAppointmentsByUserId(userId?: string) {
+    if (!userId) {
+      throw new UnauthorizedException();
+    }
+    return await this.appointmentsRepository.findByUserId(userId);
   }
 }
